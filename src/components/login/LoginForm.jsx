905f06c91a15a59c4895ec63b1a88334ee212496
Rain Tomista <rstomista@gmail.com>
2018-06-07 05:23:17 +0800

    Added signup button with label

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -5,10 +5,22 @@ const LoginForm = (props) => (
   <div className="col-md-8 d-flex login-form">
     <div className="container align-self-center">
       <h1>Know what's happening in your Barangay!</h1>
+
+      {/* Username and Password */}
       <form>
         <LoginInputField type="text" name="username" label="Username" />
         <LoginInputField type="password" name="password" label="Password" />
       </form>
+
+      {/* Sign-up */}
+      <div className="d-flex justify-content-center">
+        <div className="sign-up">
+          <label>Don't have an account yet?</label>
+          <button class="btn rounded">Sign-up</button>
+        </div>
+      </div>
+
+      {/* Partners */}
       <div className="partners d-flex justify-content-center">
         <img src="images/DICT.png" alt="" className="align-self-center" />
         <img src="images/DOST.jpg" alt="" className="align-self-center" />
