c683a4f39a5c6a9e09f98884f003acdba7d1c39f
Rain Tomista <rstomista@up.edu.ph>
2018-06-09 02:48:32 +0800

    Implemented signup form using formix

--- src/components/signup/SignUpInputField.jsx
+++ src/components/signup/SignUpInputField.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
-const SignupInputField = (props) => (
-    <div className="inline-fluid form-group">
+const SignupInputField = (props) => {
+  const { required, touched, errors } = props;
+
+  return (
+    <div>
+      <div className="inline-fluid form-group">
         <label htmlFor={props.name}>{props.label}</label>
-        <input type={props.type} className="form-control" name={props.name} />
+        <input
+          type={props.type}
+          className="form-control"
+          name={props.name}
+          value={props.value}
+          onChange={props.handleChange}
+          onBlur={props.handleBlur}
+        />
+      </div>
+      { required ? touched && errors && <div>{errors}</div> : errors && <div>{errors}</div> }
     </div>
-);
+  );
+}
 
 export default SignupInputField;
--- src/containers/SignUp.jsx
+++ src/containers/SignUp.jsx
@@ -3,12 +3,121 @@ import NavBar from 'components/common/NavBar';
 import SignUpInputField from 'components/signup/SignUpInputField';
 import SignUpSlider from 'components/signup/SignUpSlider';
 import 'stylesheets/containers/SignUp.css';
+import { withFormik, Field } from 'formik';
+import Yup from 'yup';
+
+const Form = props => {
+  const { handleChange, handleBlur, handleSubmit, errors, touched } = props;
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="section">
+        <SignUpInputField
+          type="text"
+          name="firstName"
+          label="First Name"
+          handleChange={handleChange}
+          handleBlur={handleBlur}
+          touched={touched.firstName}
+          errors={errors.firstName}
+          required
+        />
+        <SignUpInputField
+          type="text"
+          name="lastName"
+          label="Last Name"
+          handleChange={handleChange}
+          handleBlur={handleBlur}
+          touched={touched.lastName}
+          errors={errors.lastName}
+          required
+        />
+        <SignUpInputField
+          type="text"
+          name="email"
+          label="Email Address"
+          handleChange={handleChange}
+          handleBlur={handleBlur}
+          touched={touched.email}
+          errors={errors.email}
+        />
+        <SignUpInputField
+          type="text"
+          name="username"
+          label="Username"
+          handleChange={handleChange}
+          handleBlur={handleBlur}
+          touched={touched.username}
+          errors={errors.username}
+        />
+        <SignUpInputField
+          type="password"
+          name="password"
+          label="Password"
+          handleChange={handleChange}
+          handleBlur={handleBlur}
+          touched={touched.password}
+          errors={errors.password}
+        />
+        <SignUpInputField
+          type="text"
+          name="mobile"
+          label="Mobile Phone"
+          handleChange={handleChange}
+          handleBlur={handleBlur}
+        />
+        <SignUpInputField
+          type="text"
+          name="landline"
+          label="Landline"
+          handleChange={handleChange}
+          handleBlur={handleBlur}
+        />
+      </div>
+      <div className="section">
+        {/* <Field component="select" name="region">
+          <option value="Region 1">Region 1</option>
+          <option value="Region 2">Region 2</option>
+        </Field> */}
+      </div>
+      <div className="text-center">
+        <button type="submit" className="btn rounded">Sign Up</button>
+      </div>
+
+      {/* {JSON.stringify(props, null, 2)} */}
+    </form>
+  );
+}
+
+const SignupForm = withFormik({
+  mapPropsToValues: () => ({
+    firstName: '',
+    lastName: '',
+    email: '',
+    username: '',
+    password: '',
+    mobile: '',
+    landline: '',
+    region: ''
+  }),
+
+  validateOnChange: true,
+  validationSchema: Yup.object().shape({
+    firstName: Yup.string().required('Required'),
+    lastName: Yup.string().required('Required'),
+    email: Yup.string().email('Invalid email address'),
+    username: Yup.string().min(3, 'Username must be at least 3 characters'),
+    password: Yup.string().min(8, 'Password must be at least 8 characters')
+  }),
+
+  handleSubmit: (values) => {
+    console.log(values);
+  },
+
+})(Form);
+
 
 export default class SignUp extends Component {
-  constructor(props) {
-    super(props);
-  }
-  
   render() {
     return (
       <div>
@@ -19,28 +128,10 @@ export default class SignUp extends Component {
               <div className="col-12 col-sm-12 col-md-12 col-lg-5 d-flex align-items-center justify-content-center">
                 <SignUpSlider />
               </div>
-              <div className="col-12 col-sm-12 col-md-12 col-lg-7 d-flex align-items-center justify-content-center">
+              <div className="col-12 col-sm-12 col-md-12 col-lg-5 d-flex align-items-center justify-content-center">
                 <div className="signup-form">
                   <h3 className="form-title">Sign Up</h3>
-                  <form>
-                    <div className="section">
-                      <SignUpInputField type="text" name="region" label="Region" />
-                      <SignUpInputField type="text" name="province" label="Province" />
-                      <SignUpInputField type="text" name="city" label="City/Municipality" />
-                      <SignUpInputField type="text" name="brgy" label="Barangay" />
-                      <SignUpInputField type="text" name="brgy-capt" label="Barangay Captain" />
-                      <SignUpInputField type="text" name="brgy-capt" label="Barangay Office Address" />
-                    </div>
-                    <div className="section">
-                      <SignUpInputField type="text" name="name" label="Name" />
-                      <SignUpInputField type="text" name="phone" label="Phone" />
-                      <SignUpInputField type="text" name="mobile" label="Mobile Phone" />
-                      <SignUpInputField type="text" name="email" label="Email" />
-                    </div>
-                    <div className="text-center">
-                      <button className="btn rounded">Sign Up</button>
-                    </div>
-                  </form>
+                  <SignupForm />
                 </div>
               </div>
             </div>
