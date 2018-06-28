3ad5f6f002ffc4cd6b020950808ead99416e1ede
giooooo <gbperalta1@up.edu.ph>
2018-06-29 00:55:23 +0800

    Added npm script for temporary deployment on surge.sh Changed react env varible production to react app env

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const API_HOST = process.env.REACT_APP_PRODUCTION === true ? process.env.REACT_APP_SERVER : 'http://localhost:8000';
+const API_HOST = process.env.REACT_APP_ENV === 'production' ? process.env.REACT_APP_SERVER : 'http://localhost:8000';
 
 export default API_HOST;
