9221af0000eb6fb7e704ba53d91e51c6849d095b
Rain Tomista <rstomista@up.edu.ph>
2018-07-26 23:19:07 +0800

    Added data to user profile expanded view

--- src/components/profile/UserProfileExpandedView.jsx
+++ src/components/profile/UserProfileExpandedView.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+/*--------------- Mobx ---------------*/
+import { observer } from 'mobx-react';
+
+/*--------------- Subcomponents ---------------*/
 import UserProfileDetails from 'components/profile/UserProfileDetails';
 
 /*--------------- FontAwesome Icons ---------------*/
@@ -8,13 +13,17 @@ import faEnvelope from '@fortawesome/fontawesome-free-solid/faEnvelope';
 import faMobileAlt from '@fortawesome/fontawesome-free-solid/faMobileAlt';
 import faPhone from '@fortawesome/fontawesome-free-solid/faPhone';
 
+/*--------------- Stylesheet  ---------------*/
 import './UserProfileExpandedView.less';
 
-const UserProfileExpandedView = (props) => {
+const UserProfileExpandedView = observer((props) => {
+  const { AppData } = props;
+  const profileData = AppData.profileData;
+
   return (
     <div className="row">
       {/* User Profile Expanded Details */}
-      <div className="col-md-9">
+      <div className="col-md-12 col-lg-9">
         <div className="row user-profile-expanded-details">
 
           {/* User Details (Left Side) */}
@@ -23,8 +32,8 @@ const UserProfileExpandedView = (props) => {
 
               {/* User Profile Picture, Full Name, and Location */}
               <img src="images/default-user.png" alt="" className="card-img" />
-              <h3 class="card-title">Juan Dela Cruz</h3>
-              <p class="card-text">Caloocan City</p>
+              <h3 className="card-title">{profileData && `${profileData.user_first_name} ${profileData.user_last_name}`}</h3>
+              <p className="card-text">{profileData && `${profileData.barangay_page_municipality}`}</p>
 
               {/* User Stats */}
               <div className="user-stats">
@@ -42,7 +51,6 @@ const UserProfileExpandedView = (props) => {
 
           {/* User Details (Right Side) */}
           <div className="col-md-8">
-            <div className="vertical-divider"></div>
 
             {/* User Additional Info */}
             <div className="additional-info">
@@ -59,42 +67,42 @@ const UserProfileExpandedView = (props) => {
                 {/* Region */}
                 <div className="row">
                   <div className="col">
-                    <span>Region</span>
+                    <span>Region: {profileData && `${profileData.barangay_page_region}`}</span>
                   </div>
                 </div>
 
                 {/* Province */}
                 <div className="row">
                   <div className="col">
-                    <span>Province</span>
+                    <span>Province: {profileData && `${profileData.barangay_page_province}`}</span>
                   </div>
                 </div>
 
                 {/* City/Municipality */}
                 <div className="row">
                   <div className="col">
-                    <span>City/Municipality</span>
+                    <span>City/Municipality: {profileData && `${profileData.barangay_page_municipality}`}</span>
                   </div>
                 </div>
 
                 {/* Barangay */}
                 <div className="row">
                   <div className="col">
-                    <span>Barangay</span>
+                    <span>Barangay: {profileData && `${profileData.barangay_page_name}`}</span>
                   </div>
                 </div>
 
                 {/* Barangay Captain */}
                 <div className="row">
                   <div className="col">
-                    <span>Barangay Captain</span>
+                    <span>Barangay Captain: {profileData && `${profileData.barangay_page_captain}`}</span>
                   </div>
                 </div>
 
                 {/* Barangay Office Address */}
                 <div className="row">
                   <div className="col">
-                    <span>Barangay Office Address</span>
+                    <span>Barangay Office Address: {profileData && `${profileData.barangay_page_office_address_street}`}</span>
                   </div>
                 </div>
               </div>
@@ -114,28 +122,41 @@ const UserProfileExpandedView = (props) => {
                     <ul className="list-group list-group-flush">
 
                       {/* User Email Address */}
-                      <li className="list-group-item">
-                        <a className="card-link">
-                          <FontAwesomeIcon icon={faEnvelope} className="icon" />
-                          <span>rstomista@up.edu.ph</span>
-                        </a>
-                      </li>
+                      {profileData && profileData.user_email && (
+                        <li className="list-group-item">
+                          <a href={'mailto:' + profileData.user_email} className="card-link">
+                            <FontAwesomeIcon icon={faEnvelope} className="icon" />
+                            <span>
+                              {profileData.user_email}
+                            </span>
+                          </a>
+                        </li>
+                      )}
 
                       {/* User Mobile Number */}
-                      <li className="list-group-item">
-                        <a className="card-link">
-                          <FontAwesomeIcon icon={faMobileAlt} className="icon" />
-                          <span>rstomista@up.edu.ph</span>
-                        </a>
-                      </li>
+                      {profileData && profileData.user_mobile_number && (
+                        <li className="list-group-item">
+                          <a href={'callto:' + profileData.user_mobile_number} className="card-link">
+                            <FontAwesomeIcon icon={faMobileAlt} className="icon" />
+                            <span>
+                              {profileData.user_mobile_number}
+                            </span>
+                          </a>
+                        </li>
+                      )}
 
                       {/* User Landline Number */}
-                      <li className="list-group-item">
-                        <a className="card-link">
-                          <FontAwesomeIcon icon={faPhone} className="icon" />
-                          <span>rstomista@up.edu.ph</span>
-                        </a>
-                      </li>
+                      {profileData && profileData.user_landline_number && (
+                        <li className="list-group-item">
+                          <a href={'callto:' + profileData.user_landline_number} className="card-link">
+                            <FontAwesomeIcon icon={faPhone} className="icon" />
+                            <span>
+                              {profileData.user_landline_number}
+                            </span>
+                          </a>
+                        </li>
+                      )}
+
                     </ul>
                   </div>
                 </div>
@@ -145,7 +166,7 @@ const UserProfileExpandedView = (props) => {
         </div>
       </div>
 
-      <div className="col-md-3">
+      <div className="col-lg-3 d-none d-lg-block d-xl-block">
         <div className="card">
           <div className="card-body">
             &nbsp;
@@ -154,6 +175,6 @@ const UserProfileExpandedView = (props) => {
       </div>
     </div>
   );
-}
+});
 
 export default UserProfileExpandedView;
