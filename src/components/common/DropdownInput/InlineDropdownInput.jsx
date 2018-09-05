ebbd70888643a0b6b5b3393c56b1ce15be471155
Rain Tomista <rstomista@up.edu.ph>
2018-09-05 23:18:59 +0800

    Implement inline dropdown input component

---
+++ src/components/common/DropdownInput/InlineDropdownInput.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+import PropTypes from 'prop-types';
+import DropdownList from 'react-widgets/lib/DropdownList'
+
+import 'react-widgets/lib/less/react-widgets.less';
+
+import './InlineDropdownInput.less';
+
+const InlineDropdownInput = observer((props) => (
+  <React.Fragment>
+    <div className="inline form-group">
+      <label htmlFor={props.id}>
+        <span>{props.label}</span>
+        <span className="required"> * </span>
+      </label>
+      <DropdownList
+        id={props.id}
+        data={props.field.options}
+        defaultValue={props.field.value}
+        onChange={props.field.sync}
+      />
+    </div>
+
+  </React.Fragment>
+));
+
+InlineDropdownInput.propTypes = {
+  id: PropTypes.string.isRequired,
+  field: PropTypes.object.isRequired,
+  label: PropTypes.string.isRequired,
+}
+
+export default InlineDropdownInput;
+
