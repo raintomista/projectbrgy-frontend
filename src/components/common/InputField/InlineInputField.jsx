4962a8334b42fdb687d20651b6620473eb111479
Rain Tomista <rstomista@up.edu.ph>
2018-09-05 22:02:59 +0800

    Update inline input field component

--- src/components/common/InputField/InlineInputField.jsx
+++ src/components/common/InputField/InlineInputField.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
+import { observer } from 'mobx-react';
 import PropTypes from 'prop-types';
 
 import './InlineInputField.less';
 
-const InlineInputField = (props) => (
-  <div className="inline form-group">
-    <label htmlFor={props.id}>{props.label}</label>
-    <input type="text" className="form-control" id={props.id} />
-  </div>
-)
+const InlineInputField = observer((props) => (
+  <React.Fragment>
+    <div className="inline form-group">
+      <label htmlFor={props.id}>
+        <span>{props.label}</span>
+        { props.required && <span className="required"> * </span> }
+      </label>
+      <input type="text" className={`${props.field.error ? 'invalid' : ''} form-control`} id={props.id} {...props.field.bind()} maxLength={props.maxLength} />
+    </div>
+
+  </React.Fragment>
+));
 
 InlineInputField.propTypes = {
   id: PropTypes.string.isRequired,
+  field: PropTypes.object.isRequired,  
   label: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
+  maxLength: PropTypes.number,
+  required: PropTypes.bool,
+  type: PropTypes.string.isRequired,  
 }
 
 export default InlineInputField;
