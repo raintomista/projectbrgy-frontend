20820df82a59f89586eabb3f017b6df22256a452
Rain Tomista <rstomista@up.edu.ph>
2018-09-11 12:53:27 +0800

    Truncate report messages in brgy members' reports page

--- src/components/brgy-reports/subcomponents/MemberReportItem.jsx
+++ src/components/brgy-reports/subcomponents/MemberReportItem.jsx
@@ -14,10 +14,15 @@ const MemberReportItem = (props) => {
           {props.committeeType && (
             <span className="committee-type">{props.committeeType} &middot; </span>
           )}
-          <span>by {props.author}</span>          
+          <span>by {props.author}</span>
           <span> &middot; {dateCreated}</span>
         </div>
-        <p className="message">{props.message}</p>
+        <p className="message">
+          {props.message.length > 80 ?
+            `${props.message.substring(0, 80)}...` :
+            props.message
+          }
+        </p>
         <div className="status">
           <div className="indicator-group"><div className="indicator"></div> {props.status}</div>
         </div>
