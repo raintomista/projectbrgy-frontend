c8904a4d3280f8c25ba3802a96953ba73c341d01
Rain Tomista <rstomista@up.edu.ph>
2018-09-15 04:17:15 +0800

    Added disabling to button loader

--- src/components/common/Loader/ButtonLoader.jsx
+++ src/components/common/Loader/ButtonLoader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './ButtonLoader.less';
 
 const ButtonLoader = (props) => (
-  <button className="button-loader" onClick={props.handleClick}>
+  <button className="button-loader" onClick={props.handleClick} disabled={props.loading}>
     { props.loading && 'Loading...'}
     { !props.loading && 'Show more'}    
   </button>
