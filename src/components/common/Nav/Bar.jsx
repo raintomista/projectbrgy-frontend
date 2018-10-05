287428e07276b08e2572ba5b6bb5e67a11f9726b
Rain Tomista <rstomista@up.edu.ph>
2018-10-05 14:18:34 +0800

    Add searchbar in mobile

--- src/components/common/Nav/Bar.jsx
+++ src/components/common/Nav/Bar.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import faSearch from '@fortawesome/fontawesome-free-solid/faSearch'
+
 import SideBar from 'components/common/SideNav/Bar';
 import SearchBar from './subcomponent/SearchBar';
 import Home from 'assets/images/nav-home.png';
@@ -13,10 +16,15 @@ export default class NavBar extends Component {
   constructor(props) {
     super(props);
     this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+    this.state = { searchHidden: true };
   }
 
   onSetSidebarOpen() {
     this.props.AppData.toggleSidebar();
+  }
+
+  showSearchbar() {
+    this.setState((prevState) => ({ searchHidden: !prevState.searchHidden }));
   }
 
   render() {
@@ -24,13 +32,53 @@ export default class NavBar extends Component {
       <div>
 
         <nav className="navbar navbar-collapse-sm navbar-dark fixed-top">
-          <div className="container">
+          {this.state.searchHidden && (
+            <div className="d-flex d-md-none d-lg-none d-xl-none container">
+              <div className="navbar-brand">
+                <a href="/dashboard" className="nav-logo">
+                  <img src={Home} alt="" />
+                </a>
+              </div>
+              <ul className="navbar-nav right">
+                <li className="nav-item">
+                  <a className="nav-search nav-link" onClick={() => this.showSearchbar()}>
+                    <FontAwesomeIcon icon={faSearch} />
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-menu nav-link" onClick={() => this.onSetSidebarOpen()}>
+                    <img src={Menu} alt="" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          )}
+          {!this.state.searchHidden && (
+            <div className="d-flex d-md-none d-lg-none d-xl-none container">
+              <ul className="navbar-nav sm-search-bar">
+                <li className="nav-item">
+                  <div className="nav-link">
+                    <SearchBar history={this.props.history} />
+                  </div>
+                </li>
+              </ul>
+              <ul className="navbar-nav right">
+                <li className="nav-item">
+                  <a className="nav-search nav-link" onClick={() => this.showSearchbar()}>
+                    <h5>Cancel</h5>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          )}
+
+          <div className="d-none d-md-flex d-lg-flex d-xl-flex container">
             <div className="navbar-brand mr-auto ">
               <a href="/dashboard" className="nav-logo">
                 <img src={Home} alt="" />
               </a>
             </div>
-            <ul className="d-none d-md-block navbar-nav">
+            <ul className="navbar-nav">
               <li className="nav-item">
                 <div className="nav-link">
                   <SearchBar history={this.props.history} />
@@ -45,6 +93,9 @@ export default class NavBar extends Component {
               </li>
             </ul>
           </div>
+
+
+
         </nav>
         <SideBar {...this.props} onSetOpen={this.onSetSidebarOpen} />
       </div>
